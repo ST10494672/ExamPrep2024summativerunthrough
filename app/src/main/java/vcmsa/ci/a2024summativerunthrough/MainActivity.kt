7a3e951822e73e442d20b88d7eef136d0ba7d629
ST10494672 <ST10494672@vcconnect.edu.za>
2025-05-27 15:57:55 +0200

    update

--- app/src/main/java/vcmsa/ci/a2024summativerunthrough/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/a2024summativerunthrough/MainActivity.kt
@@ -1,8 +1,10 @@
 package vcmsa.ci.a2024summativerunthrough
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        //variables
+        val goButton=findViewById<AppCompatButton>(R.id.goButton)
+        val exitButton =findViewById<AppCompatButton>(R.id.exitButton)
+
+        //On click listener
+        goButton.setOnClickListener
+        val intent = Intent(this,WeatherActivity::class.java)
+        startActivity(Intent)
+        }
+        exitbutton.setOnClickListener {
+            finishAffinity() // close the app
         }
     }
 }
--- app/src/main/java/vcmsa/ci/a2024summativerunthrough/WeatherActivity.kt
+++ app/src/main/java/vcmsa/ci/a2024summativerunthrough/WeatherActivity.kt
@@ -16,5 +16,43 @@ class WeatherActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Variables
+        //Arrays
+        val daysOfTheWeek = arrayOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+        val minTemperature = intArrayOf(15,17,19,20,16,15,18)
+        val maxTemperature = intArrayOf(23,25,26,27,30,26,27)
+        val weatherConditions = arrayOf("Cloudy","Sunny","Windy","Hot","Rainy","Stormy","Foggy")
+
+        //Views
+        val averageTempTxt = findViewById<TextView>(R.id.averageTempTxt)
+        val detailedViewBtn = findViewById<Button>(R.id.detailedViewBtn)
+        val exitButton = findViewById<Button>(R.id.exitButton)
+
+        val averageTempValue = getAverageTemperature(minTemperature,maxTemperature)
+        averageTempTxt.text = "Average Temperature: ${averageTempValue}C"
+
+        detailedViewBtn.setOnClickListener{
+            val intent = Intent(this,DetailedActivity::class.java)
+            intent.putExtra("days",daysOfTheWeek)
+            intent.putExtra("minTemp",minTemperature)
+            intent.putExtra("maxTemp",maxTemperature)
+            intent.putExtra("conditions",weatherConditions)
+            startActivity(intent)
+        }
+
+    }
+    fun getAverageTemperature(minTemp: IntArray, maxTemps: IntArray):Double {
+        var totalMin = 0
+        var totalMax = 0
+
+        for (i in minTemp.indices){
+            totalMin += minTemp[i]
+            totalMax += maxTemps[i]
+        }
+        val averageMin = totalMin/minTemp.size
+        val averageMax = totalMax/ maxTemps.size
+
+        return (averageMin + averageMax)/2.0
+
     }
 }
